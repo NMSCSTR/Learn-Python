7a780f543ff32c606d0b4f3fffb5aa89af2650b9
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-01-27 21:31:51 +0800

    day1 at night studying node with prev and next pointers

--- node.py
+++ node.py
@@ -1,10 +1,12 @@
 class Node:
     def __init__(self, data):
         self.data = data  # Store the data
+        self.prev = None  # Pointer to the previous node
         self.next = None  # Pointer to the next node
 
 node1 = Node(10)
 node2 = Node(20)
-node1.next = node2  # Linking node1 to node2
-print(node1.data)  # Output: 10
-print(node1.next.data)  # Output: 20
+node1.next = node2
+node2.prev = node1
+print(node1.next.data)
+print(node2.prev.data)
