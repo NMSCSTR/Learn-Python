31a43d6e620bdac268070c5200e8b1374723dc79
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-01-27 14:41:26 +0800

    day1

---
+++ day1.py
@@ -0,0 +1,44 @@
+#Python Syntax Recap
+
+#Comments
+#single line comment
+
+
+#Indentation
+if 5 > 4:
+    print("5 is greater than 4")
+else:
+    print("4 is greater than 5")
+
+#Variables and Data Types
+integer = 10
+floating = 10.5
+string = "Hello World"
+boolean = True
+list = ["true",2,3,4,5]
+tuple = (1,2,3,4,5)
+set = {1,2,3,4,5}
+dictionary = {"name":"John","age":30}
+print(integer)
+print(floating)
+print(string.lower())
+print(boolean)
+print(list)
+print(tuple)
+print(set)
+print(dictionary)
+
+#Type Conversion
+name = int("7")
+name = float(name)
+print(name)
+print(type(name))
+#Operators
+#Arithmetic Operators +, -, *, /, %, **, //
+#Comparison Operators ==, !=, >, <, >=, <=
+#Logical Operators and, or, not
+#Assignment Operators =, +=, -=, *=, /=, %=, **=, //=
+
+#Collections List, Tuple, Set, Dictionary
+#Control Structures if, elif, else, for, while
+#Loops
