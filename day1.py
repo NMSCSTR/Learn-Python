b6fdb7a62a2e7a3f1b74f9fc5ab0d5ed597060d6
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-01-30 16:23:48 +0800

    removing all lines of codes

--- day1.py
+++ day1.py
@@ -1,26 +1,13 @@
 #Python Syntax Recap
-
 #Comments
-#single line comment
-
-
 #Indentation
 #Variables and Data Types
-number = 7.7
-
-
 #Type Conversion
-print("Original type:", number)
-converted = int(number)
-print("Type:", str(converted))
-
 #Operators
-
 #Arithmetic Operators +, -, *, /, %, **, //
 #Comparison Operators ==, !=, >, <, >=, <=
 #Logical Operators and, or, not
 #Assignment Operators =, +=, -=, *=, /=, %=, **=, //=
-
 #Collections List, Tuple, Set, Dictionary
 #Control Structures if, elif, else, for, while
 #Loops
