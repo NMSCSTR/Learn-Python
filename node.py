59337b4f3137d97b8b8503c4c5a4e1c389aebd4d
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-02-02 23:24:51 +0800

    removing

--- node.py
+++ node.py
@@ -4,9 +4,3 @@ class Node:
         self.prev = None  # Pointer to the previous node
         self.next = None  # Pointer to the next node
 
-node1 = Node(10)
-node2 = Node(20)
-node1.next = node2
-
-print(node1.next.data)
-print(node2.prev.data)
