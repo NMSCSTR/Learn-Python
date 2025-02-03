017a82fb60e27dedf4359230d0747d59519e1a99
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-02-03 13:28:56 +0800

    type conversion

--- debugging.py
+++ debugging.py
@@ -10,3 +10,37 @@ for i in range(numbers):
 if x = 10:
     print("x is 10")
 """
+
+
+#Predict the Output
+"""
+x = 5
+y = "5"
+print(x * 2)
+print(y * 2)
+
+"""
+
+#Fill in the Blanks
+"""
+# Define a list with three fruits
+fruits = ["apple", "banana", ____ ]
+
+# Print the first fruit
+print(fruits[ ____ ])
+
+# Use a for loop to print each fruit
+for ____ in fruits:
+    print(____)
+
+"""
+
+#Implement a conditional statement
+"""
+age = int(input("Enter your age: "))
+if ____:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
+"""
