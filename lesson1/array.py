a5167c1f8a310239eb1363cf42eadeef1471c616
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-02-04 23:40:52 +0800

    array

---
+++ lesson1/array.py
@@ -0,0 +1,27 @@
+#Using lists(Dynamic Arrays)
+array = [10, 20, 30, 40, 50]
+print(array)
+
+#Accessing elements
+print(array[0])
+
+# Modifying elements
+array[1] = 25
+print(array)
+
+# Adding elements
+array.append(60) #add at the end 
+array.insert(2, 15)
+print(array)
+
+#Removing elements
+array.pop() # remove the last element
+array.remove(15) # Remove first occurence of 15
+print(array)
+
+#checking existence
+print(30 in array)
+
+#Iterating through an array
+for num in array:
+    print(num, end=" ")
