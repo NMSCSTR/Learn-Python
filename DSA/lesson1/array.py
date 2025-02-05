adf171a1a804f5bfb59e51dfdcb6a1e6a84ac718
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-02-05 22:17:32 +0800

    Class discussion

--- DSA/lesson1/array.py
+++ DSA/lesson1/array.py
@@ -1,27 +1,25 @@
 #Using lists(Dynamic Arrays)
-array = [10, 20, 30, 40, 50]
-print(array)
-
+array = [10, 40, 20, 30, 40, 50]
 #Accessing elements
-print(array[0])
+print(array[2]) # specific elements to access or to be printed
 
 # Modifying elements
-array[1] = 25
-print(array)
+array[2] = 14 # Specific elements to be modified using index
 
 # Adding elements
-array.append(60) #add at the end 
-array.insert(2, 15) # Inserts at index 2
-print(array)
+array.append(60) # Insert value to the last elements
+array.insert(2, 2025) # Has 2 parameter to pass which 1st: index 2nd: Value
 
 #Removing elements
-array.pop() # remove the last element
-array.remove(15) # Remove first occurence of 15
-print(array)
+array.pop() # The last element of our list will be removed
+array.remove(40) #Ang kina unhan ra nga elements ang ma remove if list have the same value
 
 #checking existence
-print(30 in array)
+print(50 in array) # True or false - Type(Boolean)
 
 #Iterating through an array
+print(array)
+
 for num in array:
-    print(num, end=" ")
+    print(num, end="\n") #New line
+
