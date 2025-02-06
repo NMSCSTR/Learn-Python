483d02cd4ed13c820bd87048816e5a71fd5dc309
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-02-06 12:41:05 +0800

    python function activity

--- Programming2/function.py
+++ Programming2/function.py
@@ -1,5 +1,10 @@
+"""
+CREATE YOUR OWN FUNCTION INLINE WITH 
+THE DIFFERENTTYPES OF FUNCTION BELOW
+"""
+# NAME OF OUR SCHOOL DEAN
 # Function with no parameter
 # Function with parameters
 # Function with default parameters
-# Functions With Multiple Return Values
+# Functions With Return Values
 # Recursive function
