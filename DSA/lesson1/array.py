e6db9e82ea72fae9231cb0b0fe60c8716a05143a
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-02-06 15:26:27 +0800

    python function activity 2

--- DSA/lesson1/array.py
+++ DSA/lesson1/array.py
@@ -1,25 +1,26 @@
+"""
+Perform built-in function in the list being provided
+based on the different processes below
+
+"""
 #Using lists(Dynamic Arrays)
-array = [10, 40, 20, 30, 40, 50]
-#Accessing elements
-print(array[2]) # specific elements to access or to be printed
-
-# Modifying elements
-array[2] = 14 # Specific elements to be modified using index
-
-# Adding elements
-array.append(60) # Insert value to the last elements
-array.insert(2, 2025) # Has 2 parameter to pass which 1st: index 2nd: Value
-
-#Removing elements
-array.pop() # The last element of our list will be removed
-array.remove(40) #Ang kina unhan ra nga elements ang ma remove if list have the same value
-
-#checking existence
-print(50 in array) # True or false - Type(Boolean)
-
-#Iterating through an array
-print(array)
-
+array = [10, 20, 40, 20, 30, 40, 50]
+#Accessing elements(5pts)
+print(array[3]) #Syntax print(list_name[index])
+#Modifying elements(5pts)
+array[2] = 231 # list_name[index] = newValue
+#Adding elements(10pts)
+array.append(45) # list_name.append(value)
+array.insert(2, 455) # list_name.insert(index, value)
+#Removing elements(10pts)
+array.pop() # list_name.pop()
+array.remove(20) # list_name.remove(element)
+#checking existence(5pts)
+print(20 in array) # print(element in list_name) True
+#Iterating through an array(10pts)
 for num in array:
-    print(num, end="\n") #New line
-
+    print(num, end="->")
+"""
+FULL NAME OF OUR 
+CICT DEAN
+"""
