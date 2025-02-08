4ffce37850d1d294ee92f1fd4a122fb66eade1ef
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-02-09 00:03:39 +0800

    TIC-TAC-TOE

--- DSA/lesson1/tic_tac_toe.py
+++ DSA/lesson1/tic_tac_toe.py
@@ -1,7 +1,24 @@
 tic_tac_toe = [
-    [None, None, None],
-    [None, None, None],
-    [None, None, None]
+     #0    #1    #2
+    [None, None, None], #0
+    [None, None, None], #1
+    [None, None, None]  #2
 ]
+# X X O
+# O X O
+# 0 X X
 
-print(tic_tac_toe)
+tic_tac_toe[0][0] = 'X'
+tic_tac_toe[0][1] = 'X'
+tic_tac_toe[0][2] = '0'
+
+tic_tac_toe[1][0] = 'O'
+tic_tac_toe[1][1] = 'X'
+tic_tac_toe[1][2] = '0'
+
+tic_tac_toe[2][0] = 'X'
+tic_tac_toe[2][1] = 'X'
+tic_tac_toe[2][2] = 'X'
+
+for i in range(len(tic_tac_toe)):
+    print(tic_tac_toe[i])
