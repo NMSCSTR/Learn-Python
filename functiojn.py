98b98029af24fdf02135fa21575f6f7df65cf829
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-02-12 15:14:52 +0800

    non modular

--- functiojn.py
+++ functiojn.py
@@ -1,5 +1,16 @@
-def factorial(n):
-    if n == 0:
-        return 1
-    return n * factorial(n-1)
-print("Factorial: ", factorial(5))
+# Modular code
+def display_score(students, scores):
+    for i in range(len(students)):
+        print(f"Students sdncfsjk: {students[i]}, scoresn  csjscjk: {scores[i]}")
+
+student = ["Alice", "Bob", "Charlie"]
+score = [85, 90, 78]
+display_score(student, score)
+
+#None modular
+student = ["Alice", "Bob", "Charlie"]
+score = [85, 90, 78]
+
+print(f"Studentssdsda : {student[0]}, scores: {score[0]}")
+print(f"Students: {student[1]}, scores: {score[1]}")
+print(f"Students: {student[2]}, scores: {score[2]}")
