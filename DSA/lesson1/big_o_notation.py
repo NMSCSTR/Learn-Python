e4ed57c7b98e5951a35de04c2aa44bb62bfc7b7f
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-03-04 19:09:58 +0800

    python big o notation

---
+++ DSA/lesson1/big_o_notation.py
@@ -0,0 +1,15 @@
+
+# BLOCKD
+# Group 1 = Library
+# Group 2 = Height Converter
+# Group 3 = Maigis Ordering
+# Group 4 = Movie Ticket Booking System
+# Group 5 = Simple Interest 
+
+
+# BLOCKB
+# Group 1 - BMI Calculator
+# Group 2 - Heigth Converter
+# Group 3 - Expense Splitter
+# Group 4 - Attendance 
+# Group 5 - Ordering
