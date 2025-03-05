ca7ae3ad18bf7476de5cb9b2a1b4ddb8f1f2f6f0
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-03-05 08:51:54 +0800

    add setters

--- Programming2/classes.py
+++ Programming2/classes.py
@@ -10,10 +10,17 @@ class Car:
     def get_car_info(self):
         print(f"This {self.brand} {self.model} was invented at {self.year}")
 
+    def set_brand(self, brand):
+        self.brand = brand
+
+    def set_model(self, model):
+        self.model = model
+
 # Creating an object
 car1 = Car("Honda", "Civic", 2007) #Creating an object
 car2 = Car("Toyota", "Mk5 Supra", 2009) #Creating an object
-
+car1.set_brand("Nissan") #Changing the brand of the car
+car1.set_model("Skyline") #Changing the model of the car
 
 #Accesing info using method inside the class
 car1.get_car_info()
