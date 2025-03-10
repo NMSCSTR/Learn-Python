c44e79bec92c2239be8885be4a2e979741e24f95
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-03-10 11:46:08 +0800

    class discussion

--- Programming2/classes.py
+++ Programming2/classes.py
@@ -1,7 +1,20 @@
-class Student:
-    def __init__(name, age):
-        name = name
-        age = age
-        
-s = Student("John", 20)
-print(s.name)  
+class Student: 
+    def __init__(self, name, gender, age, address):
+        self.name = name #ADELINE
+        self.gender = gender #FEMALE
+        self.age = age #18
+        self.address = address #LANAO
+
+    def get_details(self): #Getters
+        print(f"Name: {self.name} Gender: {self.gender}")
+
+# Create an objects
+student1 = Student("ADELINE", "FEMALE", 18, "LANAO") 
+student2 = Student("BOBORDS", "FEMALE", 18, "NAMIK") 
+print(student1.name) #Output: ADELINE
+student1.get_details() #Output: Name: ADELINE
+
+# Student
+# Initializations
+# Methods kung CLASS function
+# Objects
