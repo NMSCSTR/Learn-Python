25d3e325c89878313f583db50b5195852e68feea
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-04-02 01:08:51 +0800

    exam

--- DSA/lesson1/sample.py
+++ DSA/lesson1/sample.py
@@ -4,7 +4,6 @@ tic_tac_toe = [
         [None, None, None] 
     ]
 #FIRST ROW WITH INDEX OF 0
-# SYNTAX 2d_list_name[row][column/elements] = new_value
 tic_tac_toe[0][0] = "X" #0
 tic_tac_toe[0][1] = "O" #1
 tic_tac_toe[0][2] = "O" #2
@@ -17,10 +16,5 @@ tic_tac_toe[2][0] = "X"
 tic_tac_toe[2][1] = "X"
 tic_tac_toe[2][2] = "X"
 
-# Add a list at the end of the list
-tic_tac_toe.append(['O','O','O'])
-print(tic_tac_toe)
-
 for rows in tic_tac_toe:
-    for elements in rows:
-        print(elements)
+        print(rows)
---
+++ Programming2/caliao.py
@@ -0,0 +1,8 @@
+# Declare the variable
+message = "Hello, Python!"
+
+# Print the length of the string
+print("Length:", len(message))
+
+# Convert to uppercase and print
+print("Uppercase:", message.upper())
