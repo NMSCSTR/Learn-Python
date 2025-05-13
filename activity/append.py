661f85fd20b3cbb5a717dc678b3843bcda87e0c4
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-05-13 12:30:59 +0800

    Oral and hands-on activity

---
+++ activity/append.py
@@ -0,0 +1,32 @@
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+
+    
+
+            
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=' -> ')
+            current = current.next
+        print("None")
+
+ll = LinkedList() #Intance
+
+# Append values to the list
+ll.append(10)   # List: 10
+ll.append(20)   # List: 10 -> 20
+ll.append(30)   # List: 10 -> 20 -> 30
+ll.append(40)   # List: 10 -> 20 -> 30 -> 40
+ll.delete()
+
+# Display the current list
+ll.display()     # Output: 10 -> 20 -> 30 -> 40 -> None
---
+++ activity/display.py
@@ -0,0 +1,45 @@
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if not self.head:
+            self.head = new_node 
+        else:
+            current = self.head 
+            while current.next:
+                current = current.next 
+            current.next = new_node 
+
+    # Method to delete the last node in the linked list
+    def delete(self):
+        if not self.head:
+            print("List is empty!")
+        elif not self.head.next:
+            self.head = None
+        else:
+            current = self.head #10
+            while current.next.next:
+                current = current.next
+            current.next = None
+
+            
+
+
+ll = LinkedList() #Intance
+
+# Append values to the list
+ll.append(10)  
+ll.append(20)   
+ll.append(30)   
+ll.append(40)   
+ll.delete()
+ll.display()   
